696b2cefd7187db3f73768f69b35dd56cf10ea64
Amanda Nunn <amandanunn@thearrayschool.com>
2018-12-13 14:10:16 -0700

    final solution

--- answer.js
+++ answer.js
@@ -1,1 +1,13 @@
 // *** YOUR ANSWER BELOW ***
+function beerSong(n){
+
+  let song = "";
+
+  for(let i = n; n > 0; n--){
+    let newN = n - 1;
+    song += `${n} bottles of beer on the wall, ${n} bottles of beer. Take one down, pass it around. ${newN} bottles of beer on the wall.`;
+  }
+  return song;
+}
+
+beerSong(99);
